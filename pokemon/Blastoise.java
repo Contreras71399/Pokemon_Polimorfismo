7a049adca8e487caa2b3a061a026650cd872107f
Contreras71399 <145361635+Contreras71399@users.noreply.github.com>
2023-10-27 23:21:05 -0600

    Update Blastoise.java

--- pokemon/Blastoise.java
+++ pokemon/Blastoise.java
@@ -2,35 +2,30 @@ package pokemon.pokemon;
 
 public class Articuno extends Pokemon {
     public Articuno(Tipo tipo, int PS, int Ataque, int Defensa, int AtaqueEspecial, int DefensaEspecial, int Velocidad, int Nivel) {
-        super(Tipo.HIELO.VOLADOR, 90, 85, 100, 95, 125, 85,10);
+        super(Tipo.AGUA, 139, 148, 100, 85, 105, 78, 54);
 
         ListaMovimientos listaMovimientos = new ListaMovimientos();
 
         Movimiento[] movimientos = new Movimiento[4];
-        movimientos[0] = listaMovimientos.buscarMovimientoPorNombre("Tornado");
-        movimientos[1] = listaMovimientos.buscarMovimientoPorNombre("Nieve Polvo");
-        movimientos[2] = listaMovimientos.buscarMovimientoPorNombre("Esquirla Helada");
-        movimientos[3] = listaMovimientos.buscarMovimientoPorNombre("Liofilizacion");
+        movimientos[0] = ListaMovimientos.MovimientoPorNombre("Foco Resplandor");
+        movimientos[1] = ListaMovimientos.MovimientoPorNombre("Pistola Agua");
+        movimientos[2] = ListaMovimientos.MovimientoPorNombre("Giro Rapido");
+        movimientos[3] = ListaMovimientos.MovimientoPorNombre("Agua Cola");
 
         setMovimientos(movimientos);
     }
 
     @Override
-    public double obtenerEfectividad(Pokemon pokemon) {
+    public double EfectividadObtenida(Pokemon pokemon) {
         double efectividad = 1.0;
 
-        //No efectivo
-        if (pokemon.getTipo() == Tipo.ROCA) efectividad = 0.0;
-        
-        //No muy efectivo
-        if (pokemon.getTipo() == Tipo.ACERO) efectividad = 0.25;
-        if (pokemon.getTipo() == Tipo.ELECTRICO) efectividad = 0.25;
-        if (pokemon.getTipo() == Tipo.FUEGO) efectividad = 0.25;
-        if (pokemon.getTipo() == Tipo.BICHO) efectividad = 0.50;
-        if (pokemon.getTipo() == Tipo.PLANTA) efectividad = 0.50;
-        
-        //Muy efectivo
-        if (pokemon.getTipo() == Tipo.TIERRA) efectividad = 4.0;
+        if (pokemon.getTipo() == Tipo.ACERO) efectividad = 0.50;
+        if (pokemon.getTipo() == Tipo.AGUA) efectividad = 0.50;
+        if (pokemon.getTipo() == Tipo.FUEGO) efectividad = 0.50;
+        if (pokemon.getTipo() == Tipo.HIELO) efectividad = 0.50;
+      
+        if (pokemon.getTipo() == Tipo.ELECTRICO) efectividad = 2.0;
+         if (pokemon.getTipo() == Tipo.PLANTA) efectividad = 2.0;
        
         return efectividad;     
     }
