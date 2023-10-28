7f3f09b0f63b27716261285d13fedc35993bbb0b
Contreras71399 <145361635+Contreras71399@users.noreply.github.com>
2023-10-27 23:44:38 -0600

    Update BatallaPokemon.java

--- BatallaPokemon.java
+++ BatallaPokemon.java
@@ -1,14 +1,14 @@
-package pokemon;
+package PokemmonPoli;
 
 import pokemon.pokemon.*;
 
 public class BatallaPokemon {
 public static void main(String[] args) {
-      Mew mewInstancia = new Mew(Tipo.PSIQUICO, 100, 100, 100, 100, 100, 100, 10);
-      Articuno articunoInstancia = new Articuno(Tipo.HIELO.VOLADOR, 90, 85, 100, 95, 125, 85, 10);
+      Mew mewInstancia = new Mew(Tipo.ACERO, 100, 100, 100, 100, 100, 100, 10);
+      Blastoise BlastoiseInstancia = new Articuno(Tipo.AGUA, 139, 148, 100, 85, 105, 78, 10);
    
-  mewInstancia.atacar(2, articunoInstancia);
-  articunoInstancia.atacar(1, mewInstancia);
+  mewInstancia.atacar(2, BlastoiseInstancia);
+  BlastoiseInstancia.atacar(1, mewInstancia);
 
   System.out.println(Mew.class.getSimpleName() + ": PS " + mewInstancia.getPS());
   System.out.println(Articuno.class.getSimpleName() + ": PS " + articunoInstancia.getPS());
