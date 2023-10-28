9d2b083e34549a5fef10a86179b6d9e1b5e5ffac
Contreras71399 <145361635+Contreras71399@users.noreply.github.com>
2023-10-27 23:31:31 -0600

    Update Gengar.java

--- pokemon/Gengar.java
+++ pokemon/Gengar.java
@@ -1,36 +1,38 @@
-package pokemon.pokemon;
+package PokemonPoli.PokemonPoli;
 
-public class Articuno extends Pokemon {
-    public Articuno(Tipo tipo, int PS, int Ataque, int Defensa, int AtaqueEspecial, int DefensaEspecial, int Velocidad, int Nivel) {
-        super(Tipo.HIELO.VOLADOR, 90, 85, 100, 95, 125, 85,10);
+public class Gengar extends Pokemon {
+    public Gengar(Tipo tipo, int PS, int Ataque, int Defensa, int AtaqueEspecial, int DefensaEspecial, int Velocidad, int Nivel) {
+        super(Tipo.FANTASMA.VENENO, 130, 125, 145, 230, 150, 180,10);
 
         ListaMovimientos listaMovimientos = new ListaMovimientos();
 
         Movimiento[] movimientos = new Movimiento[4];
-        movimientos[0] = listaMovimientos.buscarMovimientoPorNombre("Tornado");
-        movimientos[1] = listaMovimientos.buscarMovimientoPorNombre("Nieve Polvo");
-        movimientos[2] = listaMovimientos.buscarMovimientoPorNombre("Esquirla Helada");
-        movimientos[3] = listaMovimientos.buscarMovimientoPorNombre("Liofilizacion");
+        movimientos[0] = ListaMovimientos. MovimientoPorNombre("Lenguetazo");
+        movimientos[1] = ListaMovimientos. MovimientoPorNombre("Bola Sombra");
+        movimientos[2] = ListaMovimientos. MovimientoPorNombre("Come Sueños");
+        movimientos[3] = listaMovimientos. MovimientoPorNombre("Puño Sombra");
 
         setMovimientos(movimientos);
     }
 
     @Override
-    public double obtenerEfectividad(Pokemon pokemon) {
+    public double  EfectividadObtenida(Pokemon pokemon) {
         double efectividad = 1.0;
 
-        //No efectivo
-        if (pokemon.getTipo() == Tipo.ROCA) efectividad = 0.0;
+        if (pokemon.getTipo() == Tipo.LUCHA) efectividad = 0.00;
+        if (pokemon.getTipo() == Tipo.NORMAL) efectividad = 0.00;
         
-        //No muy efectivo
-        if (pokemon.getTipo() == Tipo.ACERO) efectividad = 0.25;
-        if (pokemon.getTipo() == Tipo.ELECTRICO) efectividad = 0.25;
-        if (pokemon.getTipo() == Tipo.FUEGO) efectividad = 0.25;
-        if (pokemon.getTipo() == Tipo.BICHO) efectividad = 0.50;
+        if (pokemon.getTipo() == Tipo.BICHO) efectividad = 0.25;
+        if (pokemon.getTipo() == Tipo.VENENO) efectividad = 0.25;
+       
+        
+        if (pokemon.getTipo() == Tipo.HADA) efectividad = 0.50;
         if (pokemon.getTipo() == Tipo.PLANTA) efectividad = 0.50;
         
-        //Muy efectivo
-        if (pokemon.getTipo() == Tipo.TIERRA) efectividad = 4.0;
+        if (pokemon.getTipo() == Tipo.FANTASMA) efectividad = 2.0;
+        if (pokemon.getTipo() == Tipo.PSIQUICO) efectividad = 2.0;
+        if (pokemon.getTipo() == Tipo.SINIESTRO) efectividad = 2.0;
+        if (pokemon.getTipo() == Tipo.TIERRA) efectividad = 2.0;
        
         return efectividad;     
     }
